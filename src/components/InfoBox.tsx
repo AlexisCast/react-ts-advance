679a2dc0513df3e4ad8b232041d4ab2352915ad6
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-05-26 15:53:09 -0600

    InfoBox with discriminated unions

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -1,11 +1,21 @@
 import { type ReactNode } from "react";
 
-type InfoBoxProps = {
-	mode: "hint" | "warning";
+type HintBoxProps = {
+	mode: "hint";
 	children: ReactNode;
 };
 
-const InfoBox = ({ mode, children }: InfoBoxProps) => {
+type WarningBoxProps = {
+	mode: "warning";
+	severity: "low" | "medium" | "high";
+	children: ReactNode;
+};
+
+type InfoBoxProps = HintBoxProps | WarningBoxProps;
+
+const InfoBox = (props: InfoBoxProps) => {
+	const { children, mode } = props;
+
 	if (mode === "hint") {
 		return (
 			<aside className="infobox infobox-hint">
@@ -14,8 +24,9 @@ const InfoBox = ({ mode, children }: InfoBoxProps) => {
 			</aside>
 		);
 	}
+	const { severity } = props;
 	return (
-		<aside className="infobox infobox-warning warning--high">
+		<aside className={`infobox infobox-warning warning--${severity}`}>
 			<h2>Warning</h2>
 			<p>{children}</p>
 		</aside>
